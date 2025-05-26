b12677cdb7fd8b2d5dff866f0fc8281615e36591
dltzk <igorukhov2015@yandex.ru>
2025-05-26 15:08:58 +0300

    fixed env problem

--- cmd/bot/bot.go
+++ cmd/bot/bot.go
@@ -4,21 +4,35 @@ import (
 	telebot "gopkg.in/telebot.v4"
 	handlers "telegram-bot/internal/delivery/handlers"
 	redis "telegram-bot/internal/infrastructure/redisclient"
-	"os"
+	"github.com/joho/godotenv"
 	"time"
 	"log"
+	"os"
 )
 
 func main() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatal("Ошибка при загрузке файла .env:", err)
+	}
 	rdb := redis.InitRedis()
 	log.Println("Redis started!")
 
+	token := os.Getenv("TELEGRAM_TOKEN")
+    if token == "" {
+        log.Fatal("TELEGRAM_TOKEN не установлен")
+    }
+
 	pref := telebot.Settings{
-		Token: os.Getenv("TELEGRAM_TOKEN"),
+		Token: token,
 		Poller: &telebot.LongPoller{Timeout: 10 * time.Second},
 	}
 
-	bot, _ := telebot.NewBot(pref)
+	bot, err := telebot.NewBot(pref)
+	if err != nil {
+    log.Fatalf("Не удалось создать бота: %v", err)
+	}
+
 	log.Println("Bot created")
 
 	bot.Handle("/ping", handlers.Ping(rdb))
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module telegram-bot
 go 1.24.2
 
 require (
+	github.com/joho/godotenv v1.5.1
 	github.com/redis/go-redis/v9 v9.8.0
 	gopkg.in/telebot.v4 v4.0.0-beta.4
 )
